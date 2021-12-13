322de013166d6f956c51d5e86f85a16daea7986e
SameerJadhav94 <jadhavdevendra40@gmail.com>
2021-12-13 22:06:18 +0530

    Added: Validation Of User Input For Registration And Login

--- App/Controller/note.controller.js
+++ App/Controller/note.controller.js
@@ -1,4 +1,5 @@
 const userService = require('../service/service.js')
+const validation = require('../utilities/validation');
 class Controller {
     register = (req, res) => {
         try {
@@ -8,21 +9,29 @@ class Controller {
                 email: req.body.email,
                 password: req.body.password
             };
+            const registerValidation = validation.authRegister.validate(user);
+            if(registerValidation.error) {
+                return res.status(400).send({
+                    success:false,
+                    message:'Wrong Input Validations',
+                    data: registerValidation
+                })
+            }
 
-                userService.registerUser(user, (error, data) => {
-                    if (error) {
-                        return res.status(400).json({
-                            success: false,
-                            message: 'User already exist',
-                        });
-                    } else {
-                        return res.status(200).json({
-                            success: true, 
-                            message: "User Registered",
-                            data: data,
-                        });
-                    }
-                });
+              userService.registerUser(user, (error, data) => {
+                  if (error) {
+                      return res.status(400).json({
+                          success: false,
+                          message: 'User already exist',
+                      });
+                  } else {
+                      return res.status(200).json({
+                          success: true, 
+                          message: "User Registered",
+                          data: data,
+                      });
+                  }
+              });
         } catch (error) {
             return res.status(500).json({
                 success: false, message: "Error While Registering",
