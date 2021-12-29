2fff01dd875d6d1621d06f92eb17589c3c136c66
SameerJadhav94 <jadhavdevendra40@gmail.com>
2021-12-29 05:58:18 +0530

    Added: Test Case To return response 400 When Gets Invalid Callback From Model

--- Test/userTest.js
+++ Test/userTest.js
@@ -655,6 +655,17 @@ describe("Get Note",()=>{
         })
         
     }))
+    it.only("should return status 400 if id does not gets authenticated, when getting invalid callback from model", (done =>{
+        const tokenCheck = Data.testData.token.unAuthToken
+        chai.request(server)
+        .get('/getNote')
+        .set({authorization: tokenCheck})
+        .end((err, res)=>{
+            res.should.have.status(400);
+            return done();
+        })
+        
+    }))
 
     
 })
