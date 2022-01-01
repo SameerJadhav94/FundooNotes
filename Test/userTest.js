9473a4cec3be5437045831ff67d6a42816b1522c
SameerJadhav94 <jadhavdevendra40@gmail.com>
2022-01-01 19:31:45 +0530

    Added: Test Case To Return 400 When Gets Invalid Response From Service.

--- Test/userTest.js
+++ Test/userTest.js
@@ -1001,4 +1001,14 @@ describe("Delete Note By Id", ()=>{
             done();
         })
     })
+    it.only("should return status 400 when does not gets valid response from service", (done)=>{
+        const tokenCheck = Data.testData.token.authToken
+        chai.request(server)
+        .delete("/deleteNoteById/61ce68fb3d2bc7aa00")
+        .set({authorization: tokenCheck})
+        .end((err, res) => {
+            res.should.have.status(400)
+            done();
+        })
+    })
 })
