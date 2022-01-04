a9b7da24497dd27482c37cd63b949060baa6934e
SameerJadhav94 <jadhavdevendra40@gmail.com>
2022-01-04 06:31:30 +0530

    Added: Test Case To Return 400 When Id Does Not Gets Validated.

--- Test/userTest.js
+++ Test/userTest.js
@@ -1193,4 +1193,14 @@ describe('Get label by id', () => {
         done();
       })
   })
+  it.only('should return Response 400 when Id Does Not Gets Validated', (done) => {
+    const tokenCheck = Data.testData.token.authToken;
+    chai.request(server)
+      .get('/getLabelById/61d2fd9d808c61d67b9')
+      .set({ authorization: tokenCheck })
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      })
+  })
 })
