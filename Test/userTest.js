a8e57c7df219e6044379ecd49633ac36bc3f4ddf
SameerJadhav94 <jadhavdevendra40@gmail.com>
2022-01-08 07:17:08 +0530

    Added: Test case To Return 200 When Clear Cache After Update.

--- Test/userTest.js
+++ Test/userTest.js
@@ -1574,4 +1574,14 @@ describe('Redis Testing', () => {
         done();
       });
   });
+  it('should return status 200 when get note from database after clearing cache', (done) => {
+    const tokenCheck = Data.testData.token.authToken;
+    chai.request(server)
+      .get('/getNoteById/61cc552d91cf0b22b7a84e44')
+      .set({ authorization: tokenCheck })
+      .end((err, res) => {
+        res.should.have.status(200);
+        return done();
+      });
+  });
 })  
